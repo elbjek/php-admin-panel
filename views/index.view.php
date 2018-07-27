0f9201e797bf600d2efa18d50f6b4a52d148cc95
elbjek <lenabjekovic@hotmail.com>
2018-07-27 16:44:13 +0200

    final push

--- views/index.view.php
+++ views/index.view.php
@@ -1,6 +1,6 @@
-<!-- <?php require "partials/header.view.php" ?>
-    <div class="col-lg-10  col-md-10 content reset ">
+<?php require "partials/header.view.php" ?>
+    <div class="col content reset ">
     <?php require "views/partials/searchbar.view.php" ?> 
 
 
-<?php require "partials/footer.view.php" ?> -->
+<?php require "partials/footer.view.php" ?>
